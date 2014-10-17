ab344ddec5e7078b566afc01cdd0548793efa378
Jen <jen@meatspac.es>
2014-10-17 13:01:53 -0700

    logging update

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ server.start(function () {
   io.on('connection', function (socket) {
     socket.on('join', function (user) {
       //socket.disconnect();
-      console.log('client connected');
+      console.log('client connected ', user);
       console.log(socket.rooms)
       socket.join(user);
     });
